3ed261f9724d591903639e3e80203017e0699914
akaziki <beself29@gmail.com>
2019-06-13 16:27:13 +0800

    添加键盘事件监听

--- events_package.js
+++ events_package.js
@@ -15,6 +15,9 @@ EventsPackage.prototype.initialize = function() {
   this.app.mouse.on(pc.EVENT_MOUSEUP, this.onMouseUp, this)
   this.app.mouse.on(pc.EVENT_MOUSEWHEEL, this.onMouseWheel, this)
 
+  this.app.keyboard.on(pc.EVENT_KEYDOWN, this.onKeyDown, this)
+  this.app.keyboard.on(pc.EVENT_KEYUP, this.onKeyUp, this)
+
   if (this.app.touch) {
     this.app.touch.on(pc.EVENT_TOUCHSTART, this.onTouchStart, this)
     this.app.touch.on(pc.EVENT_TOUCHEND, this.onTouchEnd, this)
@@ -58,6 +61,8 @@ EventsPackage.prototype.pickInstance = function(event) {
   }
 }
 
+//mouse
+
 EventsPackage.prototype.onMouseMove = function(event) {
   this.pickInstance(event)
 }
@@ -71,6 +76,16 @@ EventsPackage.prototype.onMouseUp = function(event) {
 }
 
 EventsPackage.prototype.onMouseWheel = function(event) {
+  this.pickInstance(event)
+}
+
+//keyboard
+
+EventsPackage.prototype.onKeyDown = function(event) {
+  this.pickInstance(event)
+}
+
+EventsPackage.prototype.onKeyUP = function(event) {
   this.pickInstance(event)
 }
 
